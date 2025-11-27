c97e6a757c6fbb1c21edba0c6666d141485c8e8a
Gleb Grigorev <gagrigorev@mai.ru>
2025-11-27 23:38:41 +0300

    finished queue

--- src/structure_data.py
+++ src/structure_data.py
@@ -30,21 +30,51 @@ class Stack:
         return self.data[self.top - 1]
 
 
-# class Queue:
-#     def __init__(self, capacity=10):
-#         self.data = [None] * capacity
-#         self.capacity = capacity
-#         self.head = 0
-#         self.tail = 0
-#         self._size = 0
-#
-#     def enquene(self, value):
-#         if self._size == self.capacity:
-#             raise OverflowError("очередь переполнена")
-#         self.data[self.tail] = value
-#         self.tail = (self.tail + 1) % self.capacity
-#         self._size += 1
-#
-#     def dequene(self):
-#
+class Queue:
+    def __init__(self, capacity=10):
+        self.data = [None] * capacity
+        self.capacity = capacity
+        self.head = 0
+        self.tail = 0
+        self._size = 0
+
+    def enquene(self, value):
+        if self._size == self.capacity:
+            raise OverflowError("очередь переполнена")
+        self.data[self.tail] = value
+        self.tail = (self.tail + 1) % self.capacity
+        self._size += 1
+
+    def dequene(self):
+        if self._size == 0:
+            raise IndexError("очередь пуста")
+        value = self.data[self.head]
+        self.data[self.head] = None
+        self.head = (self.head + 1) % self.capacity
+        self._size -= 1
+        return value
+
+    def is_empty(self):
+        return self._size == 0
+
+    def size(self):
+        return self._size
+
+    def peek(self):
+        if self._size == 0:
+            raise IndexError("очередь пуста")
+        return self.data[self.head]
+
+my_stack = Stack(5)
+my_stack.push(3)
+my_stack.push(4)
+my_stack.push(5)
+my_stack.push(6)
+my_stack.push(7)
+print(my_stack.pop())
+
+
+
+
+
 
